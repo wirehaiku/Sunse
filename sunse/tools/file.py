0d15de39c71cd01dee1e11d0f64ee464c19782a9
Stephen Malone <mail@wirehaiku.org>
2023-03-11 05:03:18 +1000

    Added sunse.tools.file module.

---
+++ sunse/tools/file.py
@@ -0,0 +1,54 @@
+"""
+File I/O functions.
+"""
+
+import glob as glob_
+import os
+
+from sunse import tools
+
+
+def create(path: str, body: str):
+    """
+    Create and write a string to a new file.
+    """
+
+    with open(path, "x", encoding="utf-8") as fobj:
+        fobj.write(tools.vals.body(body))
+
+
+def ensure(path: str):
+    """
+    Create a file path's parent directory if it does not exist.
+    """
+
+    dire = os.path.dirname(path)
+    if not os.path.isdir(dire):
+        os.makedirs(dire)
+
+
+def glob(dire: str, ptrn: str) -> list[str]:
+    """
+    Return a list of all file paths in a directory matching a glob pattern.
+    """
+
+    path = os.path.join(dire, ptrn)
+    return sorted(glob_.glob(path))
+
+
+def read(path: str) -> str:
+    """
+    Return the contents of a file as a string.
+    """
+
+    with open(path, "r", encoding="utf-8") as fobj:
+        return tools.vals.body(fobj.read())
+
+
+def write(path: str, body: str):
+    """
+    Write a string to a new or existing file.
+    """
+
+    with open(path, "w", encoding="utf-8") as fobj:
+        fobj.write(tools.vals.body(body))
---
+++ tests/test_tools/test_file.py
@@ -0,0 +1,56 @@
+"""
+Tests for 'sunse.tools.file'.
+"""
+
+import pytest
+from sunse.tools import file
+
+
+def test_create(tmp_path):
+    # setup
+    path = tmp_path / "file.ext"
+
+    # success
+    file.create(str(path), "body")
+    assert path.read_text() == "body"
+
+
+def test_ensure(tmp_path):
+    # setup
+    dire = tmp_path / "dire"
+    path = dire / "file.ext"
+
+    # success
+    file.ensure(path)
+    assert dire.exists()
+
+
+def test_glob(tmp_path):
+    # setup
+    for name in ["alpha", "bravo", "charlie"]:
+        tmp_path.joinpath(f"{name}.ext").write_text("")
+
+    # success
+    assert file.glob(str(tmp_path), "*.ext") == [
+        str(tmp_path / "alpha.ext"),
+        str(tmp_path / "bravo.ext"),
+        str(tmp_path / "charlie.ext"),
+    ]
+
+
+def test_read(tmp_path):
+    # setup
+    path = tmp_path / "file.ext"
+    path.write_text("body")
+
+    # success
+    assert file.read(str(path)) == "body"
+
+
+def test_write(tmp_path):
+    # setup
+    path = tmp_path / "file.ext"
+
+    # success
+    file.write(str(path), "body")
+    assert path.read_text() == "body"
