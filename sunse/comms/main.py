10a2fdbceefb0e4b6a328a60bc010556662180f7
Stephen Malone <mail@wirehaiku.org>
2023-03-11 16:11:19 +1000

    Improved envvar checking.

--- sunse/comms/main.py
+++ sunse/comms/main.py
@@ -8,13 +8,19 @@ from sunse.items.book import Book
 
 
 @click.group()
-@click.option("--dir", envvar="SUNSE_DIR", required=True)
-@click.option("--ext", envvar="SUNSE_EXT", required=True)
+@click.option("--dir", envvar="SUNSE_DIR", hidden=True)
+@click.option("--ext", envvar="SUNSE_EXT", hidden=True)
 @click.pass_context
 def group(ctx: click.Context, dir: str, ext: str):
     """
     Sunse: Stephen's Unseen Servant.
     """
+
+    if not dir:
+        raise click.ClickException("SUNSE_DIR not defined.")
+
+    if not ext:
+        raise click.ClickException("SUNSE_EXT not defined.")
 
     if not getattr(ctx, "obj", None):
         ctx.obj = Book(tools.path.expand(dir), ext)
