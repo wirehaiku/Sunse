475a281ad47034ced71c11ceb48fb7fd5675131c
Stephen Malone <mail@wirehaiku.org>
2023-03-11 13:02:04 +1000

    Fixed typo.
    Re #3.

--- sunse/__init__.py
+++ sunse/__init__.py
@@ -4,4 +4,4 @@ Package definition for 'sunse'.
 
 VERSION_DATE = "2023-03-11"
 VERSION_NUMS = "0.0.0"
-VERSION_TEXT = f"Soske version {VERSION_NUMS} ({VERSION_DATE})."
+VERSION_TEXT = f"Sunse version {VERSION_NUMS} ({VERSION_DATE})."
